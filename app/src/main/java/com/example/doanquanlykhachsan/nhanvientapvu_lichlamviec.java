1bc32f3f6d5b8e8503ff43307633d2b1fa107811
Duyhotboiz <duy6a9@gmail.com>
2021-10-21 22:22:23 +0700

    Duy tạo nhanvientapvu_lichlamviec

---
+++ app/src/main/java/com/example/doanquanlykhachsan/nhanvientapvu_lichlamviec.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlykhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nhanvientapvu_lichlamviec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nhanvientapvu_lichlamviec);
+    }
+}
