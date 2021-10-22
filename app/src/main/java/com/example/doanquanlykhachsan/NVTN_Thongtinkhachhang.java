8f9f409d2a57d905de4434ebf297212d0f27983e
phuc1712 <lehoangphuc.tdc2018@gmail.com>
2021-10-22 11:19:42 +0700

    Phuc tạo NVTN_Thôngtinkhachhang

---
+++ app/src/main/java/com/example/doanquanlykhachsan/NVTN_Thongtinkhachhang.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlykhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NVTN_Thongtinkhachhang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nvtn_thongtinkhachhang);
+    }
+}
