d18db6c4198cad9df67ab551306419169b8e2bae
phuc1712 <lehoangphuc.tdc2018@gmail.com>
2021-10-22 08:34:52 +0700

    Phuc tạo NVTN_SuaKhachHang

---
+++ app/src/main/java/com/example/doanquanlykhachsan/NVTN_SuaKhachHang.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlykhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NVTN_SuaKhachHang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nvtn_sua_khach_hang);
+    }
+}
