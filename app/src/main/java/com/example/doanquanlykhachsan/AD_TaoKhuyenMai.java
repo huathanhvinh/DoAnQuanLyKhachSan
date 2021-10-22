418ecd069fb9bbe12546cfc349f50e76ecf2a804
huathanhvinh <vinh.huathanh01@gmail.com>
2021-10-22 11:12:10 +0700

    Sửa giao diện lần 1

---
+++ app/src/main/java/com/example/doanquanlykhachsan/AD_TaoKhuyenMai.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlykhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AD_TaoKhuyenMai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_tao_khuyen_mai);
+    }
+}
