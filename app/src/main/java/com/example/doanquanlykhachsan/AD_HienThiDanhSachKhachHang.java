d5bc52bd947a63b273f4c200a0a582eee7eb1c3b
huathanhvinh <vinh.huathanh01@gmail.com>
2021-10-25 09:50:33 +0700

    Sửa giao diện lần 4

---
+++ app/src/main/java/com/example/doanquanlykhachsan/AD_HienThiDanhSachKhachHang.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlykhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AD_HienThiDanhSachKhachHang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_hien_thi_danh_sach_khach_hang);
+    }
+}
---
+++ app/src/main/java/com/example/doanquanlykhachsan/AD_ThemDichVu.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlykhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AD_ThemDichVu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_them_dich_vu);
+    }
+}
