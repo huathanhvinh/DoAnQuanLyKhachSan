5c020e919da9b196cc2339608e65b4bc5f596c67
PhamChiCong-ios <impero233@gmail.com>
2021-10-27 12:12:52 +0700

    đã thêm fragment phần nhân viên thu ngân_chọn phòng lập phiếu thuê

---
+++ app/src/main/java/com/example/doanquanlykhachsan/Fragment_f2.java
@@ -0,0 +1,18 @@
+package com.example.doanquanlykhachsan;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class Fragment_f2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_listroom_floor2,container,false);
+    }
+}
