3c4c134eb5c097627d199393bbae15c767d5a138
phuc1712 <lehoangphuc.tdc2018@gmail.com>
2021-10-27 11:55:47 +0700

    Phúc Tạo màn hình menu NVTN

---
+++ app/src/main/java/com/example/doanquanlykhachsan/NVTN_MenuNhanVienThuNgan.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlykhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NVTN_MenuNhanVienThuNgan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nvtn_menu_nhan_vien_thu_ngan);
+    }
+}
