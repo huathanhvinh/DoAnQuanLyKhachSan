eff7f2290758744370e6aa28d14a0049a3c7a73e
Duyhotboiz <duy6a9@gmail.com>
2021-11-02 16:34:22 +0700

    Duy lấy thông tin ca làm của nhân viên từ firebase

--- app/src/main/java/com/example/doanquanlykhachsan/nhanvientapvu_lichlamviec.java
+++ app/src/main/java/com/example/doanquanlykhachsan/nhanvientapvu_lichlamviec.java
@@ -1,23 +1,59 @@
 package com.example.doanquanlykhachsan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.doanquanlykhachsan.helpers.StaticConfig;
+import com.example.doanquanlykhachsan.model.NhanVien;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 public class nhanvientapvu_lichlamviec extends AppCompatActivity {
     Button btnTroVe;
+    TextView tvTenNhanVien,tvCaLam;
+    EditText etGhiChu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nhanvientapvu_lichlamviec);
         setControl();
         setEvent();
+        khoiTao();
+    }
+    //Lấy thông tim ca làm của nhân viên
+    private void khoiTao() {
+        StaticConfig.mNhanVien.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    NhanVien nv = ds.getValue(NhanVien.class);
+                    if (nv.getSoDienThoai().toString().equals(StaticConfig.currentphone)) {
+                        tvTenNhanVien.setText(nv.getTenNV());
+                        tvCaLam.setText(nv.getCaLam());
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void setControl() {
         btnTroVe = findViewById(R.id.btnTroVe);
+        tvTenNhanVien = findViewById(R.id.tvTenNhanVien);
+        tvCaLam = findViewById(R.id.tvCaLam);
+        etGhiChu = findViewById(R.id.etGhiChu);
     }
     private void setEvent() {
         btnTroVe.setOnClickListener(new View.OnClickListener() {
