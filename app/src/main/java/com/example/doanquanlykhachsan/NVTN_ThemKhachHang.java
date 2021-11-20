95bebcbbe7b28122db1624a386cd0affc604e29f
dangpham3040 <dangphamk18@gmail.com>
2021-11-20 20:47:48 +0700

    fix them kh

--- app/src/main/java/com/example/doanquanlykhachsan/NVTN_ThemKhachHang.java
+++ app/src/main/java/com/example/doanquanlykhachsan/NVTN_ThemKhachHang.java
@@ -26,10 +26,11 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.StorageTask;
 
 public class NVTN_ThemKhachHang extends AppCompatActivity {
-    Button btnThemKH,btnTroVe;
-    EditText edtTenKH,edtSoDT,edtDiaChi,edtCMND,edtEmail;
+    Button btnThemKH, btnTroVe;
+    EditText edtTenKH, edtSoDT, edtDiaChi, edtCMND, edtEmail;
     TextView tvMaKH;
     int maxSTT = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,38 +49,30 @@ public class NVTN_ThemKhachHang extends AppCompatActivity {
         btnThemKH.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String tenkh =edtTenKH.getText().toString();
-                String diachi=edtDiaChi.getText().toString();
-                String sodt=edtSoDT.getText().toString();
-                String cmnd=edtCMND.getText().toString();
-                String email=edtEmail.getText().toString();
-                if(!tenkh.isEmpty()&&!diachi.isEmpty()&&!sodt.isEmpty()&&!tenkh.isEmpty())
-                {
+                String tenkh = edtTenKH.getText().toString();
+                String diachi = edtDiaChi.getText().toString();
+                String sodt = edtSoDT.getText().toString();
+                String cmnd = edtCMND.getText().toString();
+                String email = edtEmail.getText().toString();
+                if (!tenkh.isEmpty() && !diachi.isEmpty() && !sodt.isEmpty() && !tenkh.isEmpty() && !email.isEmpty()) {
                     String key = StaticConfig.mKhachHang.push().getKey();
-                    KhachHang kh = new KhachHang(maxSTT,key,tenkh,sodt,diachi,cmnd);
+                    KhachHang kh = new KhachHang(maxSTT, key, tenkh, sodt, diachi, cmnd);
                     StaticConfig.mKhachHang.child(key).setValue(kh);
                     Toast.makeText(getApplicationContext(), "Bạn Đã Thêm Thành Công", Toast.LENGTH_SHORT).show();
-                    edtTenKH.setText("");
-                    edtDiaChi.setText("");
-                    edtSoDT.setText("");
-                    edtCMND.setText("");
-                    edtEmail.setText("");
-                    edtTenKH.requestFocus();
-                    maxSTT= maxSTT+1;
-                    tvMaKH.setText("KH"+maxSTT);
-                    register(email,email);
-                }
-                else
-                {
+                    maxSTT = maxSTT + 1;
+                    tvMaKH.setText("KH" + maxSTT);
+                    register(email, email);
+                } else {
                     Toast.makeText(getApplicationContext(), "Nhập Thiếu Thông Tin", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
     }
+
     private void register(String Email, String Pass) {
         //check email already exist or not.
-        StaticConfig.fAuth.fetchSignInMethodsForEmail(edtEmail.getText().toString())
+        StaticConfig.fAuth.fetchSignInMethodsForEmail(Email)
                 .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
                     @Override
                     public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
@@ -97,7 +90,6 @@ public class NVTN_ThemKhachHang extends AppCompatActivity {
                                 }
                             });
 
-
                         } else {
                             Toast.makeText(getApplicationContext(), "email da ton tai", Toast.LENGTH_SHORT).show();
                         }
@@ -105,13 +97,21 @@ public class NVTN_ThemKhachHang extends AppCompatActivity {
                     }
                 });
     }
+
     private void UpdateUser() {
         String id = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        User user = new User(id, edtEmail.getText().toString(), edtSoDT.getText().toString(), edtEmail.getText().toString(),edtCMND.getText().toString(),4);
+        User user = new User(id, edtEmail.getText().toString(), edtSoDT.getText().toString(), edtEmail.getText().toString(), edtCMND.getText().toString(), 4);
         StaticConfig.mUser.child(id).setValue(user);
+        edtTenKH.setText("");
+        edtDiaChi.setText("");
+        edtSoDT.setText("");
+        edtCMND.setText("");
+        edtEmail.setText("");
+        edtTenKH.requestFocus();
     }
+
     private void setconTrol() {
-        tvMaKH=findViewById(R.id.tvMaKH);
+        tvMaKH = findViewById(R.id.tvMaKH);
         edtDiaChi = findViewById(R.id.edtDiachi);
         edtSoDT = findViewById(R.id.edtSoDT);
         edtCMND = findViewById(R.id.edtCMND);
@@ -127,12 +127,11 @@ public class NVTN_ThemKhachHang extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                for (DataSnapshot ds: snapshot.getChildren())
-                {
+                for (DataSnapshot ds : snapshot.getChildren()) {
                     maxSTT = ds.child("stt").getValue(int.class);
                 }
-                maxSTT= maxSTT+1;
-                tvMaKH.setText("KH"+maxSTT);
+                maxSTT = maxSTT + 1;
+                tvMaKH.setText("KH" + maxSTT);
             }
 
             @Override
